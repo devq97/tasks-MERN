f178aa7c2af022681880b79aa96497ded36a2018
David Esteban Vargas Quevedo <david.vargas@decameron.com>
2020-02-25 13:36:59 -0500

    Se importa React y se llaman los componentes desde el app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from "./components/auth/Login";
+import Projects from "./components/proyects/Projects";
+import NewAccount from "./components/auth/NewAccount";
 
 function App() {
   return (
